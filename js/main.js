a63aa307a5b60d78c442752b1408b56afe753f41
Javieer E <65838690+Javieer57@users.noreply.github.com>
2022-08-06 10:18:06 -0500

    Add static map with leftlet

--- js/main.js
+++ js/main.js
@@ -23,3 +23,11 @@ async function renderIpInfo() {
 }
 
 renderIpInfo();
+
+/* :: Leftlet :: */
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '© OpenStreetMap',
+}).addTo(map);
