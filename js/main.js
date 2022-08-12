dfa63032b5b8241e1534b33da6d4bba5809485b2
Javieer E <65838690+Javieer57@users.noreply.github.com>
2022-08-11 21:01:18 -0500

    Add function to fetch (fake) data

--- js/main.js
+++ js/main.js
@@ -1,33 +1,65 @@
-async function getIpInfo() {
-	let url = '../mock-api-data.json';
+/* HTML elements */
+const ipField = document.getElementById('ip-address');
+const locationField = document.getElementById('location');
+const timezoneField = document.getElementById('timezone');
+const ispField = document.getElementById('isp');
+
+//  '../mock-api-data-2.json';
+//  '../mock-api-data.json';
+
+async function getIpAddressInfo(ip) {
+	// let url = '../mock-api-data.json';
+	let url = ip;
+
 	try {
-		let res = await fetch(url);
-		return await res.json();
+		let response = await fetch(url);
+
+		if (!response.ok) {
+			const message = `An error has occured: ${response.status}`;
+			throw new Error(message);
+		}
+
+		let data = await response.json();
+		return data;
 	} catch (error) {
 		console.log(error);
 	}
 }
 
-async function renderIpInfo() {
-	let ip = await getIpInfo();
+// getIpAddressInfo().then((data) => console.log(data));
+const updatePage = (data) => {
+	const lat = data.location.lat;
+	const lng = data.location.lng;
 
-	let ipAddress = document.getElementById('ip-address'),
-		location = document.getElementById('location'),
-		timezone = document.getElementById('timezone'),
-		isp = document.getElementById('isp');
+	updateInfoFields(data);
+	updateMap(lat, lng);
+};
 
-	ipAddress.innerText = ip.ip;
-	location.innerText = `${ip.location.city}, ${ip.location.region}, ${ip.location.postalCode}`;
-	timezone.innerText = ip.location.timezone;
-	isp.innerText = ip.isp;
+function updateInfoFields(info) {
+	let ipInfo = info;
+
+	ipField.innerText = ipInfo.ip;
+	locationField.innerText = `${ipInfo.location.city}, ${ipInfo.location.region}, ${ipInfo.location.postalCode}`;
+	timezoneField.innerText = ipInfo.location.timezone;
+	ispField.innerText = ipInfo.isp;
 }
 
-renderIpInfo();
-
 /* :: Leftlet :: */
-var map = L.map('map').setView([51.505, -0.09], 13);
+const updateMap = (lat, lng) => {
+	var map = L.map('map').setView([lat, lng], 15);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '© OpenStreetMap',
-}).addTo(map);
+	var myIcon = L.icon({ iconUrl: '../images/icon-location.svg' });
+
+	L.marker([lat, lng], { icon: myIcon }).addTo(map);
+
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 19,
+		attribution: '© OpenStreetMap',
+	}).addTo(map);
+};
+
+const searchIpInfo = (ip) => {
+	getIpAddressInfo(ip)
+		.then((data) => updatePage(data))
+		.catch((error) => error.message);
+};
